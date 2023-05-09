5d1654a0d53779e9c265e1b2571666c94a8c66a2
Harsh Gupta <91787765+DevilsN3st@users.noreply.github.com>
2023-05-09 13:03:57 +0530

    fixing map location part2

--- client/src/components/location/DefaultMap.jsx
+++ client/src/components/location/DefaultMap.jsx
@@ -11,33 +11,27 @@ import {
 } from "react-azure-maps";
 import { AuthenticationType, data } from "azure-maps-control";
 
-let option = {
-  authOptions: {
-    authType: AuthenticationType.subscriptionKey,
-    subscriptionKey: process.env.REACT_APP_AZURE_KEY, // Your subscription key
-  },
-  zoom: 15,
-  view: "Auto",
-};
-let option2={};
-
-function DefaultMap({coordinates}) {
-
-  console.log("map", coordinates)
-  useEffect(() => {
-    option.center = [coordinates.longitude, coordinates.latitude];
-    option2.position = [coordinates.longitude, coordinates.latitude];
-    console.log(option.center);
-  }, []);
-
+function DefaultMap({ coordinates }) {
+  let option = {
+    authOptions: {
+      authType: AuthenticationType.subscriptionKey,
+      subscriptionKey: process.env.REACT_APP_AZURE_KEY, // Your subscription key
+    },
+    center: [coordinates.longitude, coordinates.latitude],
+    zoom: 15,
+    view: "Auto",
+  };
+  let option2 = { position: [coordinates.longitude, coordinates.latitude] };
 
   return (
     <AzureMapsProvider>
-      <div style={{ height: "300px", width: "300px" }}>
-        {option.center && <AzureMap options={option} >
-          <AzureMapHtmlMarker options = {option2}/>
-        </AzureMap>}
-      </div>
+      {option.center && (
+        <div style={{ height: "300px", width: "300px" }}>
+          <AzureMap options={option}>
+            <AzureMapHtmlMarker options={option2} />
+          </AzureMap>
+        </div>
+      )}
     </AzureMapsProvider>
   );
 }
